50365bf314ec6683d9e9e929f082b1332675fbba
ADiao <1077833972@qq.com>
2024-04-16 16:14:33 +0800

    feat: ✨redux添加持久化

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import './App.scss';
+import "./App.scss";
 import { HashRouter } from "react-router-dom";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistor } from "./store";
 import Router from "@/routers/index";
 
 function App() {
   return (
-   <HashRouter>
-    <Router></Router>
-   </HashRouter>
+    <HashRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <Router></Router>
+      </PersistGate>
+    </HashRouter>
   );
 }
 
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,16 +1,33 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/es/storage";
+
 import counterSlice from "./features/counterSlice";
 import movieSlice from "./features/movieSlice";
 import globalSlice from "./modules/global";
+import userSlice from "./modules/user";
+
+const persistConfig = {
+  key: "root", // 持久化的 key
+  storage, // 指定持久化存储的方式，默认为 localStorage
+  whitelist: ["global", "user"] // 指定哪些 reducer 需要持久化
+};
+
+const rootReducer = combineReducers({
+  counter: counterSlice,
+  movie: movieSlice,
+  global: globalSlice,
+  user: userSlice
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
-  reducer: {
-    counter: counterSlice,
-    movie: movieSlice,
-    global: globalSlice
-  }
+  reducer: persistedReducer
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+
+export const persistor = persistStore(store);
 export default store;
